672657c03c8150a376a424c6d912dc1fd7ea1193
umarbashirr <umarbashir93@outlook.com>
2024-06-15 14:09:47 +0530

    chore: Add .gitignore and implement access token verification middleware and properties routes

---
+++ src/middlewares/verify-access-token.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const verifyAccessToken = (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.token || req.headers["x-access-token"];
+
+  console.log(token);
+
+  if (!token) {
+    return res.status(403).json({ message: "Token is required" });
+  }
+
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET!, (err: any, user: any) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });
+};
